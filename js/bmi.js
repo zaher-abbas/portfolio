92695fe41c53d7c2a0a74aa999646d270b860f20
Zaher ABBAS <z.abbas83@yahoo.com>
2025-05-28 14:25:29 +0200

    Logique de l'appli IMC est faite, il ne reste que des retouches graphiques...

--- js/bmi.js
+++ js/bmi.js
@@ -1,11 +1,79 @@
+let btnLancer = document.getElementById("btnLancer");
+let form = document.getElementById("form");
+let divResultat = document.createElement("div");
+let modalBody = document.querySelector(".modal-body");
+
+btnLancer.addEventListener("click", () => {
+    divResultat.removeAttribute("class");
+    form.style.display = "block";
+})
+
+
 function calcIMC(taille, poids) {
-
     if (poids == 0 || taille == 0) return null;
+    taille /= 100;
 
-    return (poids / taille * taille);
+    return poids / (taille * taille);
 }
 
-let form = document.getElementById("form");
+function afficherResultat(imc) {
+    form.style.display = "none";
+    modalBody.appendChild(divResultat);
+    divResultat.classList.add("text-center");
+    if (imc <= 16) {
+        divResultat.innerHTML = "<span>Anorexie ou dénutrition</span>"
+        divResultat.classList.add("anorexie");
+        return
+    }
+    else if (imc >= 16.5 && imc < 18.5) {
+        divResultat.innerHTML = "<span>Maigreur</span>"
+        divResultat.classList.add("maigreur");
+
+        return
+
+    }
+    else if (imc >= 18.5 && imc <= 25) {
+        divResultat.innerHTML = "<span>Corpulence normale</span>"
+        divResultat.classList.add("normal");
+
+        return
+
+    }
+
+    else if (imc > 25 && imc <= 30) {
+        divResultat.innerHTML = "<span>Surpoids</span>"
+        divResultat.classList.add("surpoids");
+
+        return
+
+    }
+
+    else if (imc > 30 && imc <= 35) {
+        divResultat.innerHTML = "<span>Obésité modérée (Classe 1)</span>"
+        divResultat.classList.add("classi");
+
+        return
+
+    }
+
+    else if (imc > 35 && imc < 40) {
+        divResultat.innerHTML = "<span>Obésité élevé (Classe 2)</span>"
+        divResultat.classList.add("classii");
+
+        return
+
+    }
+
+    else if (imc > 40) {
+        divResultat.innerHTML = "<span>Obésite morbide ou massive</span>"
+        divResultat.classList.add("classiii");
+
+        return
+
+    }
+}
+
+
 
 let taille = document.getElementById("taille");
 let poids = document.getElementById("poids");
@@ -30,8 +98,8 @@ btnCalcul.addEventListener("click", () => {
     else errorMsgPoids.textContent = "";
 
     if (taille.value > 0 && poids.value > 0) {
-        form.style.display = "none";
         imc = calcIMC(parseFloat(taille.value), parseFloat(poids.value));
+        afficherResultat(imc);
 
     }
 })
